2eabbaf53548e73a01acea6911382ea1b2c5c524
ayrton <ayrton.parkinson1@gmail.com>
2024-07-25 06:54:25 -0400

    Initial Commit

---
+++ Day60Challenge/Day60ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60ChallengeApp.swift
+//  Day60Challenge
+//
+//  Created by Ayrton Parkinson on 2024/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Day60ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
